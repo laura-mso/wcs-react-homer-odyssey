13ca4ca04401e6fbf08f96dfca6d32bfeef74f8e
laura-mso <laura.socha@gmx.net>
2019-06-03 16:31:12 +0200

    laura_sochaczewski_030

---
+++ backend/routes/auth/auth.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/signup', function(req, res, next) {
+  res.send('I am in POST route: auth/signup');
+});
+
+module.exports = router;
