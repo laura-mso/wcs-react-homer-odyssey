cbc37fd2e8ef76f886cf91a5ffd77f9c3c72de4d
laura-mso <laura.socha@gmx.net>
2019-06-03 12:22:27 +0200

    sochaczewski_laura_020

--- frontend/homer-odyssey/src/components/Signup.js
+++ frontend/homer-odyssey/src/components/Signup.js
@@ -1,24 +1,83 @@
 import React from 'react';
+import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
 
 export default class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'E-Mail',
+      firstname: 'James',
+      lastname: 'Bond',
+      email: 'my@email.com',
+      password: 'Passw0rd',
     };
     this.updateEmailField = this.updateEmailField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   updateEmailField(event) {
     this.setState({
-      email: event.target.value,
+      [event.target.name]: event.target.value,
     });
+  }
+
+  handleSubmit() {
+    console.log(this.state);
   }
   render() {
     return (
-      <div>
-        <h1>{this.state.email}</h1>
-        <input onChange={this.updateEmailField} type='email' name='email' />
-      </div>
+      <Form>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <FormGroup>
+          <Label for='firstname'>Firstname</Label>
+          <Input
+            onChange={this.updateEmailField}
+            type='text'
+            name='firstname'
+            id='firstname'
+            placeholder='Firstname'
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='lastname'>Lastname</Label>
+          <Input
+            onChange={this.updateEmailField}
+            type='text'
+            name='lastname'
+            id='lastname'
+            placeholder='Lastname'
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='email'>Email</Label>
+          <Input
+            onChange={this.updateEmailField}
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='password1'>Password</Label>
+          <Input
+            onChange={this.updateEmailField}
+            type='password'
+            name='password'
+            id='password1'
+            placeholder='Password'
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='password2'>Repeat password</Label>
+          <Input
+            onChange={this.updateEmailField}
+            type='password'
+            name='password'
+            id='password2'
+            placeholder='Repeat password'
+          />
+        </FormGroup>
+        <Button onClick={this.handleSubmit}>Submit</Button>
+      </Form>
     );
   }
 }
